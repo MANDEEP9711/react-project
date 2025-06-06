05dc5e66fcff0601532e10f5d4bc9cbb15d467b4
MANDEEP9711 <mandeepsingh971159@gmail.com>
2025-06-06 19:42:53 +0530

    changes loaded

--- react-file/src/App.jsx
+++ react-file/src/App.jsx
@@ -4,12 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const name = 'mandeep singh'
+  const userobj={studentname:"mandeep",age:29}
+  const userarr=["mandeep", "singh", "kumar", "rajput"]
+  let x=10;
+  let y=20;
+  function func(){
+    return "hello world"; 
+  }
+  function sum(a, b,op) {
+    if(op=="+") return a + b;
+    else if(op=="-") return a - b;
+    else if(op=="*") return a * b;
+    else return a / b;
+  }
+  let imgel = "src/images/download.jpg";
   return (
     <>
-      <h1 className='bg-green-500 w-30 rounded-sm m-0.5 p-1'>react project</h1>
-      <p className='bg-blue-200'>using react and tailwind</p>
+       <div>
+        <h1 className="bg-green-200 text-blue-500">jsx with curly braces</h1>
+        <h2 className='bg-amber-200 m-2 w-35 rounded-3xl p-3'>{name?name:"user not found"}</h2>
+        <button className='rounded-2xl p-1 bg-amber-100 text-blue-300 cursor-pointer' onClick={func}>click on</button>
+        <p>{sum(x,y,"*")} {name?name:"user not found"} {func()}</p>
+        <p>name: {userobj.studentname}</p>
+        <p>age: {userobj.age}</p>
+        <p>user array: {userarr.map((index) => <span>{index} </span>)}</p>
+        <img src={imgel} className='rounded-xl'/>
+        <input type="text" placeholder={name} className='border-1 rounded-sm text-blue-300' />
+       </div>
     </>
   )
 }
